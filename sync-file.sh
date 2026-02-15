3ffd2e612f068dbfb1e072d55082f3677ac60f45
szuryuu <ilhamdzaky2007@gmail.com>
2026-02-15 23:24:15 +0700

    refactor: removing unused file

--- sync-file.sh
+++ sync-file.sh
@@ -6,3 +6,7 @@ for item in "${files[@]}"; do
 	rsync -av --delete ~/.config/"$item"/ "$item"/
 	echo "Synced $item"
 done
+
+sed -i 's/"api-key": ".*"/"api-key": "API_KEY"/' zed/settings.json
+
+echo "DONE: sanitized zed api key in repository"
