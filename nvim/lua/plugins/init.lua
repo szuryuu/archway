dcd031d41d155a744918fc06494d487d9ef80b43
szuryuu <ilhamdzaky2007@gmail.com>
2026-06-25 14:25:56 +0700

    update

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -71,28 +71,40 @@ return {
   -- 		},
   -- 	},
   -- },
-
   {
     "yetone/avante.nvim",
+    event = "VeryLazy",
     lazy = false,
+    version = false,
     build = "make",
+    opts = {
+      provider = "copilot",
+      auto_suggestions_provider = "copilot",
+      providers = {
+        copilot = {
+          endpoint = "https://api.githubcopilot.com",
+          timeout = 30000,
+        },
+      },
+
+      behaviour = {
+        auto_suggestions = false,
+        auto_set_highlight_group = true,
+        auto_set_keymaps = true,
+        auto_apply_diff_after_generation = false,
+        support_paste_from_clipboard = false,
+      },
+      history = {
+        max_tokens = 4096,
+      },
+    },
     dependencies = {
+      "stevearc/dressing.nvim",
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
+      "hrsh7th/nvim-cmp",
+      "nvim-tree/nvim-web-devicons",
     },
-    config = function()
-      require("avante").setup {
-        provider = "copilot",
-        providers = {
-          copilot = {
-            model = "gpt-5.2-codex",
-          },
-        },
-        behaviour = {
-          auto_apply_diff_after_generation = false,
-        },
-      }
-    end,
   },
   {
     "nvim-telescope/telescope.nvim",
